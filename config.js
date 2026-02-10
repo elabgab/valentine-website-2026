b1dc90f11bd1775ce77a25739a7962305939238d
elabgab <gabrielibanez0912@gmail.com>
2026-02-11 00:31:04 +0800

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://jumpshare.com/s/hWVE4oI4xCDoAfzfOUpG", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dahlgnyqe/video/upload/v1770740648/undefined_hahlit.mp4", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
